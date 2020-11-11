86a8058c25c8e6f4802ab0f2edc3278f1834b249
Akmal Anvarov <akmalanvaroff@gmail.com>
2020-11-12 04:56:04 +0500

    add table

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.schema import Column, Table
-from sqlalchemy.types import String, Integer
+from sqlalchemy.schema import Column, Table, ForeignKey
+from sqlalchemy.types import String, Integer, Float
+from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -25,3 +26,35 @@ class Employee(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
+class Menu(db.Model):
+    __tablename__ = 'menus'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), nullable=False)
+
+    items = relationship('MenuItem', back_populates='menu')
+
+
+class MenuItem(db.Model):
+    __tablename__ = 'menu_items'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    price = Column(Float, nullable=False)
+    menu_id = Column(Integer, ForeignKey('menus.id'), nullable=False)
+    menu_type_id = Column(
+        Integer, ForeignKey('menu_item_types.id'), nullable=False)
+    type = relationship('MenuItemType')
+    menu = relationship('Menu', back_populates='items')
+
+
+class MenuItemType(db.Model):
+    __tablename__ = 'menu_item_types'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+
+class Table(db.Model):
+    __tablename__ = 'tables'
+    id = Column(Integer, primary_key=True)
+    number = Column(Integer, nullable=False, unique=True)
+    capacity = Column(Integer, nullable=False)
